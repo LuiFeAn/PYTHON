3f9bdcb3993dc09c639afd7d89177b73b485df9f
luis <luisfernandogvv@gmail.com>
2022-12-15 19:40:09 -0300

    feat: Criando pasta variáveis

---
+++ variaveis/string.py
@@ -0,0 +1,18 @@
+# STRING
+
+string = 'Olá, mundo'
+
+# Encontrar substring
+
+sub_string_result = string.find('mundo');
+
+# Substituindo string
+
+replace_string_result = string.replace('mundo','world');
+
+print({
+    string,
+    sub_string_result,
+    replace_string_result
+});
+
