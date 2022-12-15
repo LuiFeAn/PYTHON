be9ca90a681eb0d9e00dfdaaae01ce6979f1c81b
luis <luisfernandogvv@gmail.com>
2022-12-15 19:54:35 -0300

    feat: :fire: Inserindo novos métodos de manipulação de strings

--- variaveis/string.py
+++ variaveis/string.py
@@ -4,15 +4,38 @@ string = 'Olá, mundo'
 
 # Encontrar substring
 
-sub_string_result = string.find('mundo');
+find_string_result = string.find('mundo');
 
 # Substituindo string
 
 replace_string_result = string.replace('mundo','world');
 
+
+# Convertendo caracteres da string para maísculas
+
+upper_string_result = string.upper();
+
+# Convertendo caracteres da string para minúsculas
+
+lower_string_result = string.lower();
+
+# Converte a primeira letra de cada palavra da string para maíscula
+
+title_string_result = string.title()
+
+string_two = '     OLA, MUNDO     '
+
+# Remove espações em branco do ínicio e do fim da string
+strip_string_result = string_two.strip()
+
+
 print({
     string,
-    sub_string_result,
-    replace_string_result
-});
+    find_string_result,
+    replace_string_result,
+    upper_string_result,
+    lower_string_result,
+    title_string_result,
+    strip_string_result,
+})
 
